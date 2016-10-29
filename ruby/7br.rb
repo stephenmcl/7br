04f6bfb4cfa063aeb9629895df2f0c4dd68ae32f
Stephen McLaughlin <stephenmclaughlin@Stephen-McLaughlins-Laptop-2.local>
2016-10-29 18:14:01 -0400

    Adding file upload demo.

--- ruby/7br.rb
+++ ruby/7br.rb
@@ -1,4 +1,4 @@
-%w[optparse facets json date time net/http rest-client awesome_print].each { |g| require g }
+%w[optparse facets json date time net/http rest-client awesome_print digest/md5].each { |g| require g }
 
 SCRIPT_DIR   = (File.absolute_path(File.dirname(__FILE__))) + '/'
 TOKEN_FILE   = SCRIPT_DIR + '../../token.txt'
@@ -14,6 +14,8 @@ optparse_main = OptionParser.new do |opts|
   opts.separator "   create_project: create a project."
   opts.separator "    list_projects: list all projects."
   opts.separator "          billing: query billing group."
+  opts.separator "           upload: upload a list of files."
+  opts.separator "   mp_upload_list: list multipart uploads."
   opts.separator "Main args:"
   opts.on('-t', '--token', "use this token (override file)") do |t|
     main_args[:token] = t
@@ -24,6 +26,10 @@ optparse_main = OptionParser.new do |opts|
   end
   opts.on('-d', '--description STRING', "Description of project.") do |d|
   	main_args[:description] = d
+  end
+  opts.separator "upload args:"
+  opts.on('-f', '--files_to_upload LIST', Array, "upload files for a project.") do |d|
+  	main_args[:files_to_upload] = d
   end
 end
 
@@ -58,4 +64,20 @@ elsif ARGV[0] == 'create_project'
 	end
 	bg = get_first_billing_group()
 	create_project({billing_group:bg, name:main_args[:project_name], description:main_args[:description]})
+elsif ARGV[0] == 'upload'
+	p = make_project_hash()
+	project_id = p[main_args[:project_name]]["id"]
+	main_args[:files_to_upload].each do |file|
+		file_size = File.size(file)
+		md5 = md5_file(file)
+		#puts("#{md5.class}")
+		puts("#{file}")
+		res = general_post({project:project_id,name:File.basename(file),size:file_size,part_size:file_size,md5:md5},'upload/multipart')
+		#res = general_post({project:project_id,name:file},'upload/multipart')
+		ap res
+	end
+elsif ARGV[0] == 'mp_upload_list'
+	stuff = general_get_multiple('upload/multipart')
+	ap stuff
 end
+ 
--- ruby/7br_functions.rb
+++ ruby/7br_functions.rb
@@ -1,3 +1,30 @@
+def make_project_hash()
+	stuff = general_get_multiple('projects')
+	return_me = Hash.new
+	stuff['items'].each do |s|
+		return_me[s['name']] = s
+	end
+	return return_me
+end
+
+def md5_file(file)
+	md5 = Digest::MD5.new
+	File.open(file).each do |line|
+		md5.update(line)
+	end
+	return md5.hexdigest
+end
+
+def general_post(params, api_path)
+	uri = URI(API_BASEPATH + '/' + api_path)
+	puts("#{params.to_json}")
+	puts("#{uri}")
+	res = RestClient.post uri.to_s, params.to_json, {'X-SBG-Auth-Token' => TOKEN, 'Content-Type' => 'application/json', 'Accept' => 'application/json'} #{content_type: :json, accept: :json}
+	puts("#{res}")
+	h = JSON.parse(res.body).to_hash if res
+	return h
+end
+
 def get_first_billing_group()
 	uri = URI(API_BASEPATH + '/' + 'billing/groups')
 	res = RestClient.get uri.to_s, {'X-SBG-Auth-Token' => TOKEN, 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
