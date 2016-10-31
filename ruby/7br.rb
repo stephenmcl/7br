4c52da50c5b9c4d7d554419cdd9a330393d7b9c1
Stephen McLaughlin <stephenmclaughlin@Stephen-McLaughlins-Laptop-2.local>
2016-10-30 21:21:43 -0400

    more progress with playing with API

--- ruby/7br.rb
+++ ruby/7br.rb
@@ -1,9 +1,11 @@
-%w[optparse facets json date time net/http rest-client awesome_print digest/md5].each { |g| require g }
+%w[optparse facets json date time net/http rest-client awesome_print digest/md5 uri].each { |g| require g }
 
 SCRIPT_DIR   = (File.absolute_path(File.dirname(__FILE__))) + '/'
 TOKEN_FILE   = SCRIPT_DIR + '../../token.txt'
 require SCRIPT_DIR + '7br_functions.rb'
 API_BASEPATH = 'https://api.sbgenomics.com/v2'
+
+BOUNDARY = "AaB03x"
 
 main_args = Hash.new
 
@@ -16,6 +18,7 @@ optparse_main = OptionParser.new do |opts|
   opts.separator "          billing: query billing group."
   opts.separator "           upload: upload a list of files."
   opts.separator "   mp_upload_list: list multipart uploads."
+  opts.separator "  abort_mp_upload: Abort a multipart upload."
   opts.separator "Main args:"
   opts.on('-t', '--token', "use this token (override file)") do |t|
     main_args[:token] = t
@@ -30,6 +33,10 @@ optparse_main = OptionParser.new do |opts|
   opts.separator "upload args:"
   opts.on('-f', '--files_to_upload LIST', Array, "upload files for a project.") do |d|
   	main_args[:files_to_upload] = d
+  end
+  opts.separator "abort_mp_upload:"
+  opts.on('-u', '--upload_id LIST', Array, "upload_ids to abort") do |u|
+  	main_args[:upload_id] = u
   end
 end
 
@@ -66,18 +73,60 @@ elsif ARGV[0] == 'create_project'
 	create_project({billing_group:bg, name:main_args[:project_name], description:main_args[:description]})
 elsif ARGV[0] == 'upload'
 	p = make_project_hash()
+	u = make_upload_status_hash()
 	project_id = p[main_args[:project_name]]["id"]
+	#initialize uploads if necessary:
 	main_args[:files_to_upload].each do |file|
+		next if u[File.basename(file)]
 		file_size = File.size(file)
 		md5 = md5_file(file)
-		#puts("#{md5.class}")
-		puts("#{file}")
 		res = general_post({project:project_id,name:File.basename(file),size:file_size,part_size:file_size,md5:md5},'upload/multipart')
-		#res = general_post({project:project_id,name:file},'upload/multipart')
 		ap res
+	end
+	#get upload url:
+	#https://api.sbgenomics.com/v2/upload/multipart/{upload_id}/part/{part_number}
+	main_args[:files_to_upload].each do |file|
+		upload_id = u[File.basename(file)]['upload_id']
+		res = general_get_multiple('upload/multipart/' + upload_id + '/part/1')
+		#puts ('curl -X PUT ' )
+		#puts("#{res}")
+		#exit
+		#this works:
+		begin
+			this_thing = RestClient.put res['url'], file
+			puts("#{this_thing}")
+		rescue RestClient::ExceptionWithResponse => e
+			puts("DOPE!")
+			puts e.response
+		end 
+		exit
+		curl_cmd = 'curl --compressed --upload-file ' + file + ' "' + res['url'] + '"'
+		curl_res = `#{curl_cmd}`
+		puts(curl_res)
+		#exit
+		begin
+			site = RestClient::Resource.new(res['url'])
+			site.put File.read(file), :content_type => 'text/plain'
+		rescue RestClient::ExceptionWithResponse => e
+			puts e.response
+		end
+	end
+	exit
+	#complete the upload:
+	main_args[:files_to_upload].each do |file|
+		upload_id = u[File.basename(file)]['upload_id']
+		#puts("#{file},#{upload_id}")
+		 res = general_post({}, 'upload/multipart' + '/' + upload_id + '/' + 'complete')
+		 #puts("#{res}")
 	end
 elsif ARGV[0] == 'mp_upload_list'
 	stuff = general_get_multiple('upload/multipart')
 	ap stuff
+elsif ARGV[0] == "abort_mp_upload"
+	main_args[:upload_id].each do |upload_id|
+		stuff = simple_delete('upload/multipart', upload_id)
+		#stuff = general_delete({upload_id:upload_id}, 'upload/multipart')
+		ap stuff
+	end
 end
  
--- ruby/7br_functions.rb
+++ ruby/7br_functions.rb
@@ -1,3 +1,24 @@
+def make_upload_status_hash()
+	stuff = general_get_multiple('upload/multipart')
+	return_me = Hash.new
+	if stuff && stuff['items']
+		stuff['items'].each do |s|
+			#puts("#{s}")
+			return_me[s['name']] = s
+		end
+	end
+	return return_me
+end
+
+def simple_delete(api_path, extra_stuff)
+	uri = URI(API_BASEPATH + '/' + api_path + '/' + extra_stuff)
+	#res = RestClient.delete uri.to_s
+	res = RestClient::Request.execute(method: :delete, url: uri.to_s, headers:  {'X-SBG-Auth-Token' => TOKEN, 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+	#h = JSON.parse(res.body).to_hash if res
+	#return h
+	return res
+end
+
 def make_project_hash()
 	stuff = general_get_multiple('projects')
 	return_me = Hash.new
@@ -15,11 +36,24 @@ def md5_file(file)
 	return md5.hexdigest
 end
 
+def general_delete(params, api_path)
+	uri = URI(API_BASEPATH + '/' + api_path)
+
+	res = RestClient::Request.execute(method: :delete, url: uri.to_s,
+		payload: params.to_json, headers:  {'X-SBG-Auth-Token' => TOKEN, 'Content-Type' => 'application/json', 'Accept' => 'application/json'})
+
+end
+
+
 def general_post(params, api_path)
 	uri = URI(API_BASEPATH + '/' + api_path)
 	puts("#{params.to_json}")
 	puts("#{uri}")
-	res = RestClient.post uri.to_s, params.to_json, {'X-SBG-Auth-Token' => TOKEN, 'Content-Type' => 'application/json', 'Accept' => 'application/json'} #{content_type: :json, accept: :json}
+	begin
+		res = RestClient.post uri.to_s, params.to_json, {'X-SBG-Auth-Token' => TOKEN, 'Content-Type' => 'application/json', 'Accept' => 'application/json'} #{content_type: :json, accept: :json}
+	rescue RestClient::ExceptionWithResponse => e
+		puts e.response
+	end
 	puts("#{res}")
 	h = JSON.parse(res.body).to_hash if res
 	return h
